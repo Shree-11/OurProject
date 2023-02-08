8cc18b3fd4c9d86e71b8f0fd074591d5faab613e
Shreeraksha <raoshree57@gmail.com>
2023-02-08 22:30:33 +0530

    created register,forgotpassword page and updated login

---
+++ forgotpassword.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>FORGOT PASSWORD</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="center1">
+            <h1>Forgot Your Password?</h1>
+            <p>We get it, stuff happens. Just enter your email address below and we'll send you a link to reset your password!</p>
+            <form method="post">
+                <div class="txt_field">
+                    <input type="text" required>
+                    <span></span>
+                    <label>Enter Email Address</label>
+                </div>
+                <input type="submit" value="Reset Password">
+            </form>
+        </div>
+    </body>
+</html>
