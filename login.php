5246578beee70a5bd3f62d62d58facd38af6ea1d
Shreeraksha <raoshree57@gmail.com>
2023-03-06 23:14:51 +0530

    created home page

--- login.php
+++ login.php
@@ -4,6 +4,25 @@
         <meta charset="utf-8">
         <title>LOGIN</title>
         <link rel="stylesheet" href="css/style.css">
+          <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
     </head>
     <body>
         <div class="center1">
@@ -20,7 +39,10 @@
                     <label>Password</label>
                 </div>
                 <div class="pass">Forgot Password?</div>
-                <input type="submit" value="Login">
+            <div class="login-container">
+            <a href="fetch_image.php" class="login-button">Login</a>
+            </div>
+                
                 <div class="signup_link">
                     Not a member? <a href="register.php">Signup</a>
                 </div>
