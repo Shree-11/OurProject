5b7bba0bb27c54b40c88863338a2b55d6573fba8
Shreeraksha <raoshree57@gmail.com>
2023-04-19 00:50:00 +0530

    authentication

--- hap/app.py
+++ hap/app.py
@@ -9,9 +9,22 @@ from glob import glob
 import pandas as pd  
 import flask
 import pickle
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for,session
+from flask_mysqldb import MySQL
+import MySQLdb
+import MySQLdb.cursors
+import re
 import statistics, requests
 app=Flask(__name__)
+
+app.secret_key = 'smvitm'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+  
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
@@ -22,19 +35,58 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return flask.render_template('login.html')
+    message = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = %s AND password = %s', (email, password, ))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['username']
+            session['email'] = user['email']
+            message = 'Logged in successfully !'
+            return render_template('main.html', message = message)
+        else:
+            message = 'Please enter correct email / password !'
+    return render_template('login.html', message = message)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return flask.render_template('signup.html')
+        message = ''
+        if request.method == 'POST' and 'name' in request.form and 'pwd' in request.form and 'email' in request.form :
+             username = request.form['name']
+             password = request.form['pwd']
+             email = request.form['email']
+             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+             cursor.execute('SELECT * FROM user WHERE email = %s', (email, ))
+             account = cursor.fetchone()
+             if account:
+                  message = 'Account already exists !'
+             elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                  message = 'Invalid email address !'
+             elif not username or not password or not email:
+                  message = 'Please fill out the form !'
+             else:
+                  cursor.execute('INSERT INTO user VALUES (NULL, %s, %s, %s)', (username, email, password, ))
+                  mysql.connection.commit()
+                  message = 'You have successfully registered !'
+        elif request.method == 'POST':
+             message = 'Please fill out the form !'
+        return render_template('signup.html',message = message)
 
-@app.route('/main', methods=['GET', 'POST'])
-def main():
-    return flask.render_template('main.html')
-
-@app.route('/forgotpassword', methods=['GET', 'POST'])
-def forgotpassword():
-    return flask.render_template('forgotpassword.html')
+# @app.route('/main', methods=['GET', 'POST'])
+# def main():
+#     return flask.render_template('main.html')
 
 @app.route('/predict',methods = ['GET','POST'])
 def predict():
